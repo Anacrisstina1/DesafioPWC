2393619d9fedd047235c6c61bcfc74de05acef9a
Ana Cristina <anacristinadasilva1511@gmail.com>
2023-07-06 02:41:57 -0300

    entrega parcial do primeiro desafio

---
+++ DesafioPWC/src/primeiroDesafio/Reverter.java
@@ -0,0 +1,25 @@
+package primeiroDesafio;
+
+import java.util.Scanner;
+
+public class Reverter {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner (System.in);
+		
+		String fraseNormal = entrada.nextLine();
+		String trim = fraseNormal.trim();
+		String[] separar = fraseNormal.split(" ");
+		
+		for (int x = separar.length -1 ; x >= 0; x-- ){
+			
+			String teste = separar[x];
+			
+			
+			System.out.print( teste);
+				
+		
+		
+		}
+	}
+
+}
---
+++ DesafioPWC/src/primeiroDesafio/seila.java
@@ -0,0 +1,10 @@
+package primeiroDesafio;
+
+
+
+
+public class seila {
+	public static void main(String[] args) {
+	    System.out.println("Hello world!");
+	  }
+}
