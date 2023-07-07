bf169150ad2b56ff403efe6642d6820da604b83f
Ana Cristina <anacristinadasilva1511@gmail.com>
2023-07-07 15:51:13 -0300

    atualização parcial terceiro código

--- DesafioPWC/src/terceiroDesafio/Palindromo.java
+++ DesafioPWC/src/terceiroDesafio/Palindromo.java
@@ -4,15 +4,31 @@ import java.util.Scanner;
 
 public class Palindromo {
    public static void main(String[] args) {
+	   
 	   Scanner entrada = new Scanner(System.in);
 	   
 	   String palavra = entrada.nextLine();
+	   
 	   String auxiliar = "";
 	   
-	   for (int x = 0; x < palavra.length(); x++ ){
+	    for (int x = 0; x < palavra.length(); x++ ){
+	    	 for (int y = x+1 ; y < palavra.length(); y++) {
+	    		 String substring = palavra.substring(x, y);
+	    		 if(palindromo(substring) && substring.length()> auxiliar.length()) {
+	    			 auxiliar = substring;
+	    			  System.out.print(auxiliar);
+	    		 }
+	    	 } 
+	    }
 	   
-		   String letra = ""+ palavra.charAt(x);
-	   System.out.println(letra);
-	   }
+   }    
+	  
+private static boolean palindromo(String palind) {
+	        for (int i = 0; i < palind.length() / 2; i++) {
+	            if (palind.charAt(i) != palind.charAt(palind.length() - 1 )) {
+	                return false;
+	            }
+	        }
+	        return true;
 }
 }
