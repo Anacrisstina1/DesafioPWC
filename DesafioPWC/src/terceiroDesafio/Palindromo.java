247c150a757237298274296ecef2ca5fe419535f
Ana Cristina <anacristinadasilva1511@gmail.com>
2023-07-07 18:09:36 -0300

    terceiro desafio finalizado

--- DesafioPWC/src/terceiroDesafio/Palindromo.java
+++ DesafioPWC/src/terceiroDesafio/Palindromo.java
@@ -11,21 +11,23 @@ public class Palindromo {
 	   
 	   String auxiliar = "";
 	   
-	    for (int x = 0; x < palavra.length(); x++ ){
+	    for (int x = 0 ; x < palavra.length(); x++ ){
 	    	 for (int y = x+1 ; y < palavra.length(); y++) {
 	    		 String substring = palavra.substring(x, y);
-	    		 if(palindromo(substring) && substring.length()> auxiliar.length()) {
+	    		 if(palindromo(substring)) {
+	    		 if(substring.length()> auxiliar.length()) {
 	    			 auxiliar = substring;
-	    			  System.out.print(auxiliar);
-	    		 }
-	    	 } 
-	    }
-	   
+	    			
+	    		 	}
+	    		 }   
+	    	 }	    	
+	    } 
+	    System.out.print(auxiliar);
    }    
 	  
-private static boolean palindromo(String palind) {
-	        for (int i = 0; i < palind.length() / 2; i++) {
-	            if (palind.charAt(i) != palind.charAt(palind.length() - 1 )) {
+   private static boolean palindromo(String palind) {
+	        for (int i = 0; i < palind.length(); i++) {
+	            if (palind.charAt(i) != palind.charAt(palind.length() - 1 -i )) {
 	                return false;
 	            }
 	        }
