00662a7694a684fa2a20dc480b5ddbf5e69e7c8c
Ana Cristina <anacristinadasilva1511@gmail.com>
2023-07-07 20:59:26 -0300

    inicio do quarto desafio

---
+++ DesafioPWC/src/quartoDesafio/Maiusculas.java
@@ -0,0 +1,14 @@
+package quartoDesafio;
+
+import java.util.Scanner;
+
+public class Maiusculas {
+	public static void main(String[] args) {
+		Scanner entra = new Scanner (System.in);
+		
+		String entrada = entra.nextLine();
+		String[] separada =  entrada.split(".!,;");
+		String auxiliar= "";
+		
+	}
+}
