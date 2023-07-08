2e6b54b8e6db6eb99e97c905f6c0af552e5b4953
Ana Cristina <anacristinadasilva1511@gmail.com>
2023-07-07 22:45:51 -0300

    atualização parcial do quarto código

--- DesafioPWC/src/quartoDesafio/Maiusculas.java
+++ DesafioPWC/src/quartoDesafio/Maiusculas.java
@@ -1,14 +1,24 @@
 package quartoDesafio;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Maiusculas {
 	public static void main(String[] args) {
-		Scanner entra = new Scanner (System.in);
+		Scanner entrada = new Scanner (System.in);
 		
-		String entrada = entra.nextLine();
-		String[] separada =  entrada.split(".!,;");
-		String auxiliar= "";
-		
-	}
+        String frase = entrada.nextLine();
+        Pattern parametros = Pattern.compile("(^|\\.\\s+|\\?\\s+|\\!\\s+)([a-z])");
+        StringBuilder auxiliar = new StringBuilder();
+        Matcher refaz = parametros.matcher(entrada);
+        while (refaz.find()) {
+        	refaz.appendReplacement(auxiliar, auxiliar.group(1)+ auxiliar.group.toUpperCase());
+        	
+        }
+        refaz.appendTail(auxiliar);
+        String resultado = refaz.toString();
+        System.out.println(resultado);
+
+    }
 }
