74431c60d6739f225f416f528b3c89d6da90493b
Ana Cristina <anacristinadasilva1511@gmail.com>
2023-07-07 23:03:10 -0300

    quarto desafio finalizado

--- DesafioPWC/src/quartoDesafio/Maiusculas.java
+++ DesafioPWC/src/quartoDesafio/Maiusculas.java
@@ -11,13 +11,15 @@ public class Maiusculas {
         String frase = entrada.nextLine();
         Pattern parametros = Pattern.compile("(^|\\.\\s+|\\?\\s+|\\!\\s+)([a-z])");
         StringBuilder auxiliar = new StringBuilder();
-        Matcher refaz = parametros.matcher(entrada);
+        
+        Matcher refaz = parametros.matcher(frase);
+        
         while (refaz.find()) {
-        	refaz.appendReplacement(auxiliar, auxiliar.group(1)+ auxiliar.group.toUpperCase());
+        	refaz.appendReplacement(auxiliar, refaz.group(1)+ refaz.group(2).toUpperCase());
         	
         }
         refaz.appendTail(auxiliar);
-        String resultado = refaz.toString();
+        String resultado = auxiliar.toString();
         System.out.println(resultado);
 
     }
